274b00c5d9f358f8f10c6ddd7822f65821ac2dac
Miguel Canedo <18181692+migcanedo@users.noreply.github.com>
2017-10-16 16:15:31 -0400

    Update testBilleteraElectronica.py

--- testBilleteraElectronica.py
+++ testBilleteraElectronica.py
@@ -1,32 +1,28 @@
-#/!usr/bin/env python
 # -*- coding: 850 -*-
 '''
 Created on 13 oct. 2017
 
-@author: Rafael Cisneros
+@author: Rafael Cisneros 13-11156
+@author: Miguel Canedo 13-10214
 '''
 
 import unittest
-from ClassBilleteraElectronica import BilleteraElectronica
-from ClassCredito import Credito
-from ClassDebito import Debito
+from billeteraElectronica import BilleteraElectronica
+from credito import Consumo
+from debito import Recarga
 
 class TestBilleteraElectronica(unittest.TestCase):
-
     def setUp(self):
-        self.billetera = BilleteraElectronica(1,"Rafael","Cisneros",24759502,123)
-
-
+        self.billetera = BilleteraElectronica(1, "Rafael", "Cisneros", 24759502, 123)
     def tearDown(self):
         self.billetera = None
 
     def testBilletera(self):
-        self.assertEqual(self.billetera.saldo, 0,"La billetera deberia tener saldo 0")
+        self.assertEqual(self.billetera.saldo(), 0,"La billetera deberia tener saldo 0")
         self.assertEqual(len(self.billetera.debitos), 0,"La billetera no deberia tener debitos")
-        self.assertEqual(len(self.billetera.debitos), 0,"La billetera no deberia tener debitos")
-        self.assertEqual(self.billetera.nombresDue§o, "Rafael","Error con los nombres del due§o de la billetera")
-        self.assertEqual(self.billetera.nombresDue§o, "Rafael","Error con los apellidos del due§o de la billetera")
-
-     if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-        unittest.main()
+        self.assertEqual(len(self.billetera.creditos), 0,"La billetera no deberia tener debitos")
+        self.assertEqual(self.billetera.nombreOwner, "Rafael","Error con los nombres del due√±o de la billetera")
+        self.assertEqual(self.billetera.apellidoOwner, "Cisneros","Error con los apellidos del due√±o de la billetera")
+    
+    def testSaldoBilletera0(self):
+        self.assertEquals(0, self.billetera.saldo(), "La billetera deberia tener saldo 0")
